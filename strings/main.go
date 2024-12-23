6b1ade56b57445f950e9af8017a68f59f48cfc23
Pawan Kumar <pawank@exeire.com>
2024-12-23 15:43:25 +0530

    Start Time Package

--- strings/main.go
+++ strings/main.go
@@ -1,12 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	var age int = 22
-	if age > 20 {
-		fmt.Println("Age is greater than 20")
-	} else {
-		fmt.Println("Age is less than 20")
-	}
+	data := "Apple,Mango,Banana,Orange,Pineapple"
+	parts := strings.Split(data, ",")
+	fmt.Println(parts[0])
+
+	str := "I am the best for 2 days for pawan for sonu"
+	count := strings.Count(str, "for")
+	fmt.Println("Count is", count)
+
+	str = "   Hello,    Go"
+	trimmed := strings.TrimSpace(str)
+	fmt.Println("Triimed values are", trimmed)
+
+	str1 := "Mani"
+	str2 := "Kumar"
+	result := strings.Join([]string{str1, str2}, ", ")
+	fmt.Println(result)
+
 }
