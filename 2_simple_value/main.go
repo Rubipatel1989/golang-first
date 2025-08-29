4eefe1f471468981c8638880412f33b511a13070
Pawan Kumar <pawank@exeire.com>
2025-08-29 10:57:22 +0530

    Study Start

---
+++ 2_simple_value/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(1 + 1)
+	fmt.Println("Hello " + "World")
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+	fmt.Println(7 / 3)
+	fmt.Println(7.0 / 3.0)
+	fmt.Println(7 % 3)
+}
