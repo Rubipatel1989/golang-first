e679047a8df7c8102b817cdbca6b9dd45ba80653
Pawan Kumar <pawank@exeire.com>
2025-09-02 07:33:59 +0530

    Study Variables

--- 3_variables/main.go
+++ 3_variables/main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	var name string = "Saroj Kumar"
+	var age = 24
+	var isAdult = true
+	dob := "May 18, 1989"
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(isAdult)
+	fmt.Println(dob)
 	fmt.Println("Hello World")
 }
