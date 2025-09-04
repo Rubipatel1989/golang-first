209148d28b891b969dfcca3fd56e1d542ded764e
Pawan Kumar <pawank@exeire.com>
2025-09-04 09:22:10 +0530

    Add Files

--- 7_switchcase/main.go
+++ 7_switchcase/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "time"
+
 func main() {
 	age := 20
 
@@ -16,4 +18,31 @@ func main() {
 		println("Your age is 20")
 	}
 
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		println("This is enjoy day")
+	case time.Monday:
+		println("This is start of the week")
+	case time.Thursday:
+		println("This is almost weekend")
+	default:
+		println("This is a hard workiing day")
+	}
+
+	whoAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case int:
+			println("I am an integer", t)
+		case string:
+			println("I am a string", t)
+		case bool:
+			println("I am a boolean", t)
+		default:
+			println("I am something else", t)
+		}
+	}
+	whoAmI(21)
+	whoAmI("Hello")
+	whoAmI(true)
+	whoAmI(3.14)
 }
