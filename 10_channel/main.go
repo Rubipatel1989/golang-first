9ae42ee6dd6436c6543131a2c764252767e97b24
pawankexeire <pawank@exeire.com>
2025-12-01 08:06:46 +0530

    Add File With Def

--- 10_channel/main.go
+++ 10_channel/main.go
@@ -1,5 +1,12 @@
 package main
 
+// A Go channel is a built-in data structure that allows two goroutines
+// to communicate and synchronize their execution.
+
+// 1. It acts as a conduit for a specific data type.
+// 2. Data is passed between goroutines using send and receive operations.
+// 3. Channels can be unbuffered (blocking until the receiver is ready)
+// 	or buffered (allowing a limited number of values to be stored).
 import (
 	"fmt"
 	"math/rand"
--- 9_goroutine/main.go
+++ 9_goroutine/main.go
@@ -1,5 +1,13 @@
 package main
 
+// A Goroutine is a lightweight, independent function execution
+// that runs concurrently with other functions in a Go program.
+
+// 1. They are Go's way of achieving concurrency.
+// 2. They are much cheaper to create than traditional operating system threads.
+// 3. Goroutines are managed by the Go runtime and multiplexed onto a small number of OS threads.
+// 4. They communicate primarily using channels.
+
 import (
 	"fmt"
 	"time"
