f655a465e6f9999fed41ff5e3106f853644c5c6f
pawankexeire <pawank@exeire.com>
2025-12-21 20:55:49 +0530

    Add files

---
+++ 9_2_goroutineEvenOdd/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var evens []int
+var odds []int
+
+func findEvens(nums []int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for _, num := range nums {
+		if num%2 == 0 {
+			evens = append(evens, num)
+		}
+	}
+}
+func findOdds(nums []int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for _, num := range nums {
+		if num%2 != 0 {
+			odds = append(odds, num)
+		}
+	}
+}
+func main() {
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	evens = nil
+	odds = nil
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go findEvens(nums, &wg)
+	go findOdds(nums, &wg)
+
+	wg.Wait()
+
+	fmt.Println("Even numbers", evens)
+	fmt.Println("Odds numbers", odds)
+}
