ac5fb071102e7157857d2f64c6e3fe2e04dfafd4
pawankexeire <pawank@exeire.com>
2026-01-03 07:49:46 +0530

    Add files

--- 05-oop/inheritance_1/main.go
+++ 05-oop/inheritance_1/main.go
@@ -3,6 +3,8 @@ package main
 import "strconv"
 
 type Education struct {
+	Name    string
+	Age     int
 	Degree  string
 	College string
 	Streem  string
@@ -10,15 +12,17 @@ type Education struct {
 }
 
 func (e Education) GetEducationDetails() string {
-	return e.Degree + " in " + e.Streem + " from " + e.College + ", Year: " + strconv.Itoa(e.Year)
+	return "Dear " + e.Name + " with age of " + strconv.Itoa(e.Age) + " Years, You passed " + e.Degree + " in " + e.Streem + " from " + e.College + ", Year: " + strconv.Itoa(e.Year)
 }
 
 func main() {
 	edu := Education{
+		Name:    "Pawan Kumar",
+		Age:     36,
 		Degree:  "Bachelor of Science",
-		College: "XYZ University",
-		Streem:  "Computer Science",
-		Year:    2022,
+		College: "UPTU",
+		Streem:  "Information Technology",
+		Year:    2010,
 	}
 
 	details := edu.GetEducationDetails()
