c722104b1ede077955986e1d406323a40fd13f76
pawankexeire <pawank@exeire.com>
2026-01-21 21:32:02 +0530

    Add files

--- 06-concurrency/goroutine_basic/main.go
+++ 06-concurrency/goroutine_basic/main.go
@@ -5,24 +5,16 @@ import (
 	"time"
 )
 
-func printNumber(n int) {
-	for i := 0; i <= 10; i++ {
+func printNumbers(n int) {
+	for i := 1; i <= 10; i++ {
 		result := n * i
-		//fmt.Println(result)
-		fmt.Printf("%d x %d = %d\n", n, i, result)
+		fmt.Println(n, "*", i, "=", result)
 	}
-
 }
+
 func main() {
-	// for i := 0; i <= 10; i++ {
-	// 	go func(val int) {
-	// 		fmt.Println(i)
-	// 	}(i)
-	// }
-	// time.Sleep(1 * time.Second)
-	// for i := 1; i <= 10; i++ {
-	// 	fmt.Println(i)
-	// }
-	go printNumber(2)
-	time.Sleep(1 * time.Second)
+	go printNumbers(3)
+	time.Sleep(2 * time.Second)
+	println("===========Start another table ============")
+	printNumbers(2)
 }
